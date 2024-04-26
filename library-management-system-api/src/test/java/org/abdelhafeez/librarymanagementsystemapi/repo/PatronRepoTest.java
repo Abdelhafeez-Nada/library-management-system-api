e332e36f3452808f66f250300680dc646ba1cce6
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-26 19:05:13 +0300

    create patron repostory test class

---
+++ library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/repo/PatronRepoTest.java
@@ -0,0 +1,8 @@
+package org.abdelhafeez.librarymanagementsystemapi.repo;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class PatronRepoTest {
+
+}
