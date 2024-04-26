656cb620f435b95e15944d5912362943113dce8f
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-26 18:48:27 +0300

    implement find all books test

--- library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/repo/BookRepoTest.java
+++ library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/repo/BookRepoTest.java
@@ -3,11 +3,15 @@ package org.abdelhafeez.librarymanagementsystemapi.repo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
+
 import org.abdelhafeez.librarymanagementsystemapi.entity.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import jakarta.persistence.Query;
 
 @DataJpaTest()
 public class BookRepoTest {
@@ -53,6 +57,22 @@ public class BookRepoTest {
         Book foundBook = bookRepo.findById(book.getId()).orElse(null);
         // assert that the retrieved book matches the saved one
         assertEquals(book, foundBook);
+    }
+
+    @Test
+    public void testFindAllBooks() {
+        // execute JPQL to count all books
+        Query query = entityManager.getEntityManager().createQuery("SELECT COUNT(b) FROM Book b");
+        Long count = (Long) query.getSingleResult();
+        // save two books
+        Book book1 = Book.builder().author("author-1").isbn("isbn-1").title("title-1").build();
+        entityManager.persist(book1);
+        Book book2 = Book.builder().author("author-2").isbn("isbn-2").title("title-2").build();
+        entityManager.persist(book2);
+        // retrieve all books from the repository
+        List<Book> books = bookRepo.findAll();
+        // assert all books have been reterived
+        assertEquals(count + 2, books.size());
     }
 
 }
