4b14e1417764ae53c4a7d6ec7f4195f78d325725
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-26 18:27:06 +0300

    inject repository and entity manger dependencies

--- library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/repo/BookRepoTest.java
+++ library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/repo/BookRepoTest.java
@@ -1,8 +1,16 @@
 package org.abdelhafeez.librarymanagementsystemapi.repo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
-@DataJpaTest
+@DataJpaTest()
 public class BookRepoTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private BookRepo bookRepo;
 
 }
