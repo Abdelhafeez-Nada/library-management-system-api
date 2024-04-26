09db2adf7899ef40a9618d1bb1c3370b3e7badb9
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-26 15:19:46 +0300

    create book jpa repository

---
+++ library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/repo/BookRepo.java
@@ -0,0 +1,10 @@
+package org.abdelhafeez.librarymanagementsystemapi.repo;
+
+import org.abdelhafeez.librarymanagementsystemapi.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepo extends JpaRepository<Book, Long> {
+
+}
