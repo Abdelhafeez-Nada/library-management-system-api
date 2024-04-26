fd0975cbf05ae57d15de210562ec9e7a2e336191
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-26 19:06:18 +0300

    inject entity manager and patron repository dependencies

--- library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/repo/PatronRepoTest.java
+++ library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/repo/PatronRepoTest.java
@@ -1,8 +1,15 @@
 package org.abdelhafeez.librarymanagementsystemapi.repo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 @DataJpaTest
 public class PatronRepoTest {
 
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private PatronRepo patronRepo;
 }
