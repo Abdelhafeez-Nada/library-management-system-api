75198791ee021bac7b534858e91cfff2e0eb4972
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-26 18:28:22 +0300

    implement insert book test

--- library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/repo/BookRepoTest.java
+++ library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/repo/BookRepoTest.java
@@ -1,5 +1,9 @@
 package org.abdelhafeez.librarymanagementsystemapi.repo;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.abdelhafeez.librarymanagementsystemapi.entity.Book;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
@@ -12,5 +16,14 @@ public class BookRepoTest {
 
     @Autowired
     private BookRepo bookRepo;
+
+    @Test
+    public void testInsertBook() {
+        // create new book and persist it
+        Book book = Book.builder().author("author").isbn("isbn").title("title").build();
+        Book savedBook = bookRepo.save(book);
+        // assert book is inserted successfully
+        assertTrue(savedBook.getId() > 0);
+    }
 
 }
