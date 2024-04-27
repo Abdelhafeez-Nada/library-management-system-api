9de2f6142cc093f6d17a9cbdb7ed861320bfb858
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-27 21:56:50 +0300

    implement MapEntityListToDtoList test

--- library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/util/BeanMapperTest.java
+++ library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/util/BeanMapperTest.java
@@ -3,10 +3,14 @@ package org.abdelhafeez.librarymanagementsystemapi.util;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 import org.abdelhafeez.librarymanagementsystemapi.entity.Book;
+import org.abdelhafeez.librarymanagementsystemapi.web.dto.RequestBookDto;
 import org.abdelhafeez.librarymanagementsystemapi.web.dto.ResponseBookDto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,19 +21,24 @@ public class BeanMapperTest {
     @Autowired
     private BeanMapper beanMapper;
 
-    @Test
-    public void testMapEntityToDto() {
-        // create new book entity
-        Book bookEntity = Book.builder()
-                .id(1l)
+    private List<Book> entityList;
+    private RequestBookDto requestBookDto;
+
+    @BeforeEach
+    public void init() {
+        entityList = creatEntityList();
+        requestBookDto = RequestBookDto.builder()
                 .author("author")
                 .title("title")
                 .isbn("isbn")
-                .publicationYear(Short.valueOf("2024"))
-                .createdAt(new Date())
-                .available(true)
-                .enabled(true)
+                .publicationYear(Short.valueOf("2022"))
                 .build();
+    }
+
+    @Test
+    public void testMapEntityToDto() {
+        // create new book entity
+        Book bookEntity = entityList.get(0);
         // map entity to dto
         ResponseBookDto dto = beanMapper.mapEntityToDto(bookEntity, ResponseBookDto.class);
         // assert that mapping done sucessfully
@@ -38,4 +47,49 @@ public class BeanMapperTest {
         assertEquals(bookEntity.getAuthor(), dto.getAuthor());
         assertEquals(bookEntity.getTitle(), dto.getTitle());
     }
+
+    @Test
+    public void testMapEntityListToDtoList() {
+        // map entity list to dto list
+        List<ResponseBookDto> dtoList = beanMapper.mapEntityListToDtoList(entityList, ResponseBookDto.class);
+        // assert that mapping done sucessfully
+        assertNotNull(dtoList);
+        assertEquals(entityList.size(), dtoList.size());
+        assertEquals(entityList.get(0).getId(), dtoList.get(0).getId());
+        assertEquals(entityList.get(0).getTitle(), dtoList.get(0).getTitle());
+        assertEquals(entityList.get(0).getIsbn(), dtoList.get(0).getIsbn());
+        assertEquals(entityList.get(0).getAuthor(), dtoList.get(0).getAuthor());
+
+        assertEquals(entityList.size(), dtoList.size());
+        assertEquals(entityList.get(1).getId(), dtoList.get(1).getId());
+        assertEquals(entityList.get(1).getTitle(), dtoList.get(1).getTitle());
+        assertEquals(entityList.get(1).getIsbn(), dtoList.get(1).getIsbn());
+        assertEquals(entityList.get(1).getAuthor(), dtoList.get(1).getAuthor());
+    }
+
+    private List<Book> creatEntityList() {
+        // create list of entity
+        Book bookEntity1 = Book.builder()
+                .id(1l)
+                .author("author-1")
+                .title("title-1")
+                .isbn("isbn-1")
+                .publicationYear(Short.valueOf("2024"))
+                .createdAt(new Date())
+                .available(true)
+                .enabled(true)
+                .build();
+        Book bookEntity2 = Book.builder()
+                .id(2l)
+                .author("author-2")
+                .title("title-2")
+                .isbn("isbn-2")
+                .publicationYear(Short.valueOf("2024"))
+                .createdAt(new Date())
+                .available(true)
+                .enabled(true)
+                .build();
+        return Arrays.asList(bookEntity1, bookEntity2);
+    }
+
 }
