5b8719f0e51d9a79fadb6aadded4393f3f3780b4
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-27 19:40:51 +0300

    inject beanMaooer

--- library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/util/BeanMapperTest.java
+++ library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/util/BeanMapperTest.java
@@ -1,8 +1,11 @@
 package org.abdelhafeez.librarymanagementsystemapi.util;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class BeanMapperTest {
 
+    @Autowired
+    private BeanMapper beanMapper;
 }
