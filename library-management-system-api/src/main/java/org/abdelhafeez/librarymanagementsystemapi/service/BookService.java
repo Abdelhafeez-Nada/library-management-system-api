5159322277ec7982653b89c2b476e61a7c3a6890
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-27 18:06:07 +0300

    add getAllBooks() to book service contract

--- library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/service/BookService.java
+++ library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/service/BookService.java
@@ -1,4 +1,11 @@
 package org.abdelhafeez.librarymanagementsystemapi.service;
 
+import java.util.List;
+
+import org.abdelhafeez.librarymanagementsystemapi.web.dto.BookDto;
+
 public interface BookService {
+
+    public List<BookDto> getAllBooks() throws Exception;
+
 }
