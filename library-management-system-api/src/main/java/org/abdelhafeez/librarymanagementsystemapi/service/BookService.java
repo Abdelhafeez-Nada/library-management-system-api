ed384f8ee9426dcae11b67d28447eef52bafc881
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-27 18:19:24 +0300

    add createBook(BookDto dto) to book service contract

--- library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/service/BookService.java
+++ library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/service/BookService.java
@@ -2,6 +2,7 @@ package org.abdelhafeez.librarymanagementsystemapi.service;
 
 import java.util.List;
 
+import org.abdelhafeez.librarymanagementsystemapi.exception.BadRequestException;
 import org.abdelhafeez.librarymanagementsystemapi.exception.ResourceNotFoundException;
 import org.abdelhafeez.librarymanagementsystemapi.web.dto.BookDto;
 
@@ -10,5 +11,7 @@ public interface BookService {
     public List<BookDto> getAllBooks() throws Exception;
 
     public BookDto getBookById(Long id) throws Exception, ResourceNotFoundException;
+
+    public BookDto createBook(BookDto dto) throws Exception, BadRequestException;
 
 }
