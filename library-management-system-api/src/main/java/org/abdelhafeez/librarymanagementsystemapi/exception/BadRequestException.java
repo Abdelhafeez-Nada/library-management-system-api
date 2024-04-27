779f3bb8ffddd9875e91367f515f7d6a7b079398
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-27 18:17:56 +0300

    create BadRequestException class

---
+++ library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/exception/BadRequestException.java
@@ -0,0 +1,7 @@
+package org.abdelhafeez.librarymanagementsystemapi.exception;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
