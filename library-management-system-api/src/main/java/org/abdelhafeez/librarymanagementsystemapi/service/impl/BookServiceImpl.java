18d5aa532080b30afdcd5c82bd3e934a90518912
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-27 18:44:18 +0300

    implement book service contract

--- library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/service/impl/BookServiceImpl.java
+++ library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/service/impl/BookServiceImpl.java
@@ -1,13 +1,51 @@
 package org.abdelhafeez.librarymanagementsystemapi.service.impl;
 
+import java.util.List;
+
+import org.abdelhafeez.librarymanagementsystemapi.exception.BadRequestException;
+import org.abdelhafeez.librarymanagementsystemapi.exception.ResourceNotFoundException;
 import org.abdelhafeez.librarymanagementsystemapi.repo.BookRepo;
+import org.abdelhafeez.librarymanagementsystemapi.service.contract.BookService;
+import org.abdelhafeez.librarymanagementsystemapi.web.dto.RequestBookDto;
+import org.abdelhafeez.librarymanagementsystemapi.web.dto.ResponseBookDto;
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
-public class BookServiceImpl {
+public class BookServiceImpl implements BookService {
 
     private final BookRepo bookRepo;
+
+    @Override
+    public List<ResponseBookDto> getAllBooks() throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllBooks'");
+    }
+
+    @Override
+    public ResponseBookDto getBookById(Long id) throws Exception, ResourceNotFoundException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getBookById'");
+    }
+
+    @Override
+    public ResponseBookDto createBook(RequestBookDto dto) throws Exception, BadRequestException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createBook'");
+    }
+
+    @Override
+    public ResponseBookDto updateBook(Long id, RequestBookDto dto)
+            throws Exception, ResourceNotFoundException, BadRequestException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateBook'");
+    }
+
+    @Override
+    public void deleteBook(Long id) throws Exception, ResourceNotFoundException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteBook'");
+    }
 }
