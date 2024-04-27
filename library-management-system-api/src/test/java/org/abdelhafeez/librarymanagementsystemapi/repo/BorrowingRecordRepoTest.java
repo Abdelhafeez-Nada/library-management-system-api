6d8d8722603553b420aa36e56ec1510a99204c84
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-27 15:39:59 +0300

    mock book & patron repositories

--- library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/repo/BorrowingRecordRepoTest.java
+++ library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/repo/BorrowingRecordRepoTest.java
@@ -2,11 +2,18 @@ package org.abdelhafeez.librarymanagementsystemapi.repo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @DataJpaTest
 public class BorrowingRecordRepoTest {
 
     @Autowired
     private BorrowingRecordRepo borrowingRecordRepo;
+
+    @MockBean
+    private BookRepo bookRepo;
+
+    @MockBean
+    private PatronRepo patronRepo;
 
 }
