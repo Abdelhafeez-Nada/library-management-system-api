bdde2e4493c6b6c9f02e4d1eef8c230cb521c57c
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-27 18:55:43 +0300

    inject moddel mapper

--- library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/util/BeanMapper.java
+++ library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/util/BeanMapper.java
@@ -1,8 +1,13 @@
 package org.abdelhafeez.librarymanagementsystemapi.util;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
+import lombok.AllArgsConstructor;
+
 @Component
+@AllArgsConstructor
 public class BeanMapper {
 
+    private final ModelMapper mapper;
 }
