d5c47120bdb218954189a91887a248f0d115277d
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-27 15:38:38 +0300

    inject borrowing record repository

--- library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/repo/BorrowingRecordRepoTest.java
+++ library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/repo/BorrowingRecordRepoTest.java
@@ -1,8 +1,12 @@
 package org.abdelhafeez.librarymanagementsystemapi.repo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
 public class BorrowingRecordRepoTest {
+
+    @Autowired
+    private BorrowingRecordRepo borrowingRecordRepo;
 
 }
