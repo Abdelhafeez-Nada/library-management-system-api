96ce2b83543b4ed5eafe86154e5f297d7dd2bad4
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-27 18:14:36 +0300

    add getBookById(Long id) to book service contract

--- library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/service/BookService.java
+++ library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/service/BookService.java
@@ -2,10 +2,13 @@ package org.abdelhafeez.librarymanagementsystemapi.service;
 
 import java.util.List;
 
+import org.abdelhafeez.librarymanagementsystemapi.exception.ResourceNotFoundException;
 import org.abdelhafeez.librarymanagementsystemapi.web.dto.BookDto;
 
 public interface BookService {
 
     public List<BookDto> getAllBooks() throws Exception;
+
+    public BookDto getBookById(Long id) throws Exception, ResourceNotFoundException;
 
 }
