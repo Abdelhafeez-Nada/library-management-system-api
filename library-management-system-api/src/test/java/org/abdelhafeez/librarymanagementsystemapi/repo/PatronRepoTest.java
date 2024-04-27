1d70c3c560cfaeffbad1136e4f46aa609be206d2
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-27 15:15:19 +0300

    implement insert patron test

--- library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/repo/PatronRepoTest.java
+++ library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/repo/PatronRepoTest.java
@@ -1,5 +1,9 @@
 package org.abdelhafeez.librarymanagementsystemapi.repo;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.abdelhafeez.librarymanagementsystemapi.entity.Patron;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
@@ -12,4 +16,13 @@ public class PatronRepoTest {
 
     @Autowired
     private PatronRepo patronRepo;
+
+    @Test
+    public void testInsertPatron() {
+        // create new patron and persist it
+        Patron patron = Patron.builder().name("John Doe").contactInfo("123456789").build();
+        Patron savedPatron = patronRepo.save(patron);
+        // assert patron is inserted successfully
+        assertTrue(savedPatron.getId() > 0);
+    }
 }
