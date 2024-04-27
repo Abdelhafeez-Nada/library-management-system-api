4f155721eb4dac8167ad7b80067754d33782feef
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-27 18:43:24 +0300

    inject book repository in book service imple

--- library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/service/impl/BookServiceImpl.java
+++ library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/service/impl/BookServiceImpl.java
@@ -1,8 +1,13 @@
 package org.abdelhafeez.librarymanagementsystemapi.service.impl;
 
+import org.abdelhafeez.librarymanagementsystemapi.repo.BookRepo;
 import org.springframework.stereotype.Service;
 
+import lombok.AllArgsConstructor;
+
 @Service
+@AllArgsConstructor
 public class BookServiceImpl {
 
+    private final BookRepo bookRepo;
 }
