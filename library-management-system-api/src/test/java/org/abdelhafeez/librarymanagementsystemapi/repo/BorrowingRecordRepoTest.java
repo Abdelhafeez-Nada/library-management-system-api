e5af55d3f292e3a524bfbefd5e1e266a317c39af
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-27 16:24:11 +0300

    implement insert borrwoing record test

--- library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/repo/BorrowingRecordRepoTest.java
+++ library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/repo/BorrowingRecordRepoTest.java
@@ -1,5 +1,14 @@
 package org.abdelhafeez.librarymanagementsystemapi.repo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.Date;
+
+import org.abdelhafeez.librarymanagementsystemapi.entity.Book;
+import org.abdelhafeez.librarymanagementsystemapi.entity.BorrowingRecord;
+import org.abdelhafeez.librarymanagementsystemapi.entity.Patron;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
@@ -14,5 +23,32 @@ public class BorrowingRecordRepoTest {
 
     @Autowired
     private PatronRepo patronRepo;
+
+    @Test
+    public void testInsertBorrowingRecord() {
+        // caretae and save bbok
+        Book book = new Book();
+        book.setTitle("Sample Book");
+        book.setAuthor("Sample Author");
+        book.setPublicationYear((short) 2020);
+        book.setIsbn("1234567890");
+        book.setAvailable(true);
+        book = bookRepo.save(book);
+        // create and save patron
+        Patron patron = new Patron();
+        patron.setName("patron-1");
+        patron.setContactInfo("john@example.com");
+        patron = patronRepo.save(patron);
+        // create and save borrowin record
+        BorrowingRecord borrowingRecord = new BorrowingRecord();
+        borrowingRecord.setBook(book);
+        borrowingRecord.setPatron(patron);
+        borrowingRecord.setBorrowingDate(new Date());
+        BorrowingRecord savedRecord = borrowingRecordRepo.save(borrowingRecord);
+        // assert that borrowin record saved successfully
+        assertNotNull(savedRecord.getId());
+        assertEquals(book.getId(), savedRecord.getBook().getId());
+        assertEquals(patron.getId(), savedRecord.getPatron().getId());
+    }
 
 }
