3f87737bac68598d71508b5510466bb5b3070aaf
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-28 17:59:15 +0300

    add getPatronById to PatronService contract

--- library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/service/contract/PatronService.java
+++ library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/service/contract/PatronService.java
@@ -2,6 +2,7 @@ package org.abdelhafeez.librarymanagementsystemapi.service.contract;
 
 import java.util.List;
 
+import org.abdelhafeez.librarymanagementsystemapi.exception.ResourceNotFoundException;
 import org.abdelhafeez.librarymanagementsystemapi.web.dto.ResponsePatronDto;
 import org.springframework.data.domain.Page;
 
@@ -10,4 +11,6 @@ public interface PatronService {
     public List<ResponsePatronDto> getAllPatrons();
 
     public Page<ResponsePatronDto> getAllPatrons(int page, int size);
+
+    public ResponsePatronDto getPatronById(Long id) throws ResourceNotFoundException;
 }
