c0cc80f2f4fbeeccef5a5ce2550ce17086609b0f
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-28 18:57:35 +0300

    add returnBook to borrowing service contract

--- library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/service/contract/BorrowingService.java
+++ library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/service/contract/BorrowingService.java
@@ -1,5 +1,10 @@
 package org.abdelhafeez.librarymanagementsystemapi.service.contract;
 
+import org.abdelhafeez.librarymanagementsystemapi.exception.BadRequestException;
+import org.abdelhafeez.librarymanagementsystemapi.exception.ResourceNotFoundException;
+
 public interface BorrowingService {
-    public void borrowBook(Long bookId, Long patronId);
+    public void borrowBook(Long bookId, Long patronId) throws BadRequestException, ResourceNotFoundException;
+
+    public void returnBook(Long bookId, Long patronId) throws BadRequestException, ResourceNotFoundException;
 }
