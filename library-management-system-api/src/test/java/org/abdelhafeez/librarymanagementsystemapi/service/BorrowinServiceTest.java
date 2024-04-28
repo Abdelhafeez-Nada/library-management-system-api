873878a9b2a717a9ce335c6d7b6a949f4156e722
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-28 22:28:10 +0300

    implement BorrowBook_Success test

--- library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/service/BorrowinServiceTest.java
+++ library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/service/BorrowinServiceTest.java
@@ -1,9 +1,22 @@
 package org.abdelhafeez.librarymanagementsystemapi.service;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.abdelhafeez.librarymanagementsystemapi.entity.BorrowingRecord;
+import org.abdelhafeez.librarymanagementsystemapi.exception.BadRequestException;
+import org.abdelhafeez.librarymanagementsystemapi.exception.ResourceNotFoundException;
 import org.abdelhafeez.librarymanagementsystemapi.repo.BorrowingRecordRepo;
 import org.abdelhafeez.librarymanagementsystemapi.service.contract.BookService;
 import org.abdelhafeez.librarymanagementsystemapi.service.contract.PatronService;
 import org.abdelhafeez.librarymanagementsystemapi.service.impl.BorrowingServiceImpl;
+import org.abdelhafeez.librarymanagementsystemapi.web.dto.ResponseBookDto;
+import org.abdelhafeez.librarymanagementsystemapi.web.dto.ResponsePatronDto;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -21,5 +34,26 @@ public class BorrowinServiceTest {
 
     @InjectMocks
     private BorrowingServiceImpl borrowingService;
+
+    @Test
+    public void testBorrowBook_Success() throws BadRequestException, ResourceNotFoundException {
+        // Mock book and patron
+        ResponseBookDto book = new ResponseBookDto();
+        book.setAvailable(true);
+        book.setEnabled(true);
+        ResponsePatronDto patron = new ResponsePatronDto();
+        patron.setEnabled(true);
+        // Mock behavior of bookService and patronService
+        when(bookService.getBookById(anyLong())).thenReturn(book);
+        when(patronService.getPatronById(anyLong())).thenReturn(patron);
+
+        // Mock borrowingRepo.save() to return null
+        when(borrowingRepo.save(any(BorrowingRecord.class))).thenReturn(null);
+        // Call the method
+        borrowingService.borrowBook(1L, 2L);
+        // Verify interactions
+        verify(borrowingRepo, times(1)).save(any(BorrowingRecord.class));
+        verify(bookService, times(1)).makeBookUnAvailable(1L);
+    }
 
 }
