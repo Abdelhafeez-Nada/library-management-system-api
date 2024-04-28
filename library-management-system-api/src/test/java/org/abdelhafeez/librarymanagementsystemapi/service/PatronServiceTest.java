e4b44cef4ff8061f07f3d134536d706ac93e1f60
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-28 18:21:45 +0300

    mock patron repo and beanmapper

--- library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/service/PatronServiceTest.java
+++ library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/service/PatronServiceTest.java
@@ -1,9 +1,18 @@
 package org.abdelhafeez.librarymanagementsystemapi.service;
 
+import org.abdelhafeez.librarymanagementsystemapi.repo.PatronRepo;
+import org.abdelhafeez.librarymanagementsystemapi.util.BeanMapper;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class PatronServiceTest {
+
+    @Mock
+    private BeanMapper beanMapper;
+
+    @Mock
+    private PatronRepo patronRepo;
 
 }
