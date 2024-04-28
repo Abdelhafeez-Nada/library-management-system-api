e5c9aa6d7137e9544cd42df6c110f3a8e960f161
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-28 21:25:47 +0300

    implement updateReturnDate

--- library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/repo/BorrowingRecordRepo.java
+++ library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/repo/BorrowingRecordRepo.java
@@ -2,9 +2,18 @@ package org.abdelhafeez.librarymanagementsystemapi.repo;
 
 import org.abdelhafeez.librarymanagementsystemapi.entity.BorrowingRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface BorrowingRecordRepo extends JpaRepository<BorrowingRecord, Long> {
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true, value = "update borrowing_records set return_date=current_timestamp where book_id=:bookId and patron_id=:patronId")
+    public int updateReturnDate(@Param("bookId") long bookId, @Param("patronId") long patronId);
 
 }
