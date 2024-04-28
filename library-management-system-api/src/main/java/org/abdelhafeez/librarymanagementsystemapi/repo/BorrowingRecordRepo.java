8858b025afafc540742f364a29a3859c5ddc3b0a
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-28 21:54:17 +0300

    implement findByBookIdAndPatronId query method

--- library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/repo/BorrowingRecordRepo.java
+++ library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/repo/BorrowingRecordRepo.java
@@ -1,5 +1,7 @@
 package org.abdelhafeez.librarymanagementsystemapi.repo;
 
+import java.util.List;
+
 import org.abdelhafeez.librarymanagementsystemapi.entity.BorrowingRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -16,4 +18,6 @@ public interface BorrowingRecordRepo extends JpaRepository<BorrowingRecord, Long
     @Query(nativeQuery = true, value = "update borrowing_records set return_date=current_timestamp where book_id=:bookId and patron_id=:patronId")
     public int updateReturnDate(@Param("bookId") long bookId, @Param("patronId") long patronId);
 
+    // query method to find borrowing records by bookId and patronId
+    List<BorrowingRecord> findByBookIdAndPatronId(Long bookId, Long patronId);
 }
