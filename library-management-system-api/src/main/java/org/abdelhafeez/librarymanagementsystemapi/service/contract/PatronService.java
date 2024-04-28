f3b95483d1223f5ce54b813e2272db3f5262f79d
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-28 17:57:00 +0300

    add getAllPatrons to patron service contract

--- library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/service/contract/PatronService.java
+++ library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/service/contract/PatronService.java
@@ -1,5 +1,10 @@
 package org.abdelhafeez.librarymanagementsystemapi.service.contract;
 
+import java.util.List;
+
+import org.abdelhafeez.librarymanagementsystemapi.web.dto.ResponsePatronDto;
+
 public interface PatronService {
 
+    public List<ResponsePatronDto> getAllPatrons();
 }
