5a860b21e15e782896927b0ddbf6afc3d8f21da3
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-28 19:00:13 +0300

    inject borrowing record repository

--- library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/service/impl/BorrowingServiceImpl.java
+++ library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/service/impl/BorrowingServiceImpl.java
@@ -2,11 +2,17 @@ package org.abdelhafeez.librarymanagementsystemapi.service.impl;
 
 import org.abdelhafeez.librarymanagementsystemapi.exception.BadRequestException;
 import org.abdelhafeez.librarymanagementsystemapi.exception.ResourceNotFoundException;
+import org.abdelhafeez.librarymanagementsystemapi.repo.BorrowingRecordRepo;
 import org.abdelhafeez.librarymanagementsystemapi.service.contract.BorrowingService;
 import org.springframework.stereotype.Service;
 
+import lombok.AllArgsConstructor;
+
 @Service
+@AllArgsConstructor
 public class BorrowingServiceImpl implements BorrowingService {
+
+    private final BorrowingRecordRepo borrowingRepo;
 
     @Override
     public void borrowBook(Long bookId, Long patronId) throws BadRequestException, ResourceNotFoundException {
