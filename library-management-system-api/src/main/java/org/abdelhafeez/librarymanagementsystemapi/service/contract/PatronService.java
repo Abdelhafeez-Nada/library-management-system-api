7b1521050ec4f8335e9c27053c06f682377d1984
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-28 17:57:52 +0300

    add getAllBooks_pagination to patron srevice contract

--- library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/service/contract/PatronService.java
+++ library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/service/contract/PatronService.java
@@ -3,8 +3,11 @@ package org.abdelhafeez.librarymanagementsystemapi.service.contract;
 import java.util.List;
 
 import org.abdelhafeez.librarymanagementsystemapi.web.dto.ResponsePatronDto;
+import org.springframework.data.domain.Page;
 
 public interface PatronService {
 
     public List<ResponsePatronDto> getAllPatrons();
+
+    public Page<ResponsePatronDto> getAllBooks(int page, int size);
 }
