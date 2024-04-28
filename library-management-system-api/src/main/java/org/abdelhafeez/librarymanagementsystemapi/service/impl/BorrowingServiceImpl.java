6efff2ac69a79805874a8ab2b3f0aa507d872eeb
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-28 21:02:16 +0300

    implement borrowBookFunctionality

--- library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/service/impl/BorrowingServiceImpl.java
+++ library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/service/impl/BorrowingServiceImpl.java
@@ -1,12 +1,18 @@
 package org.abdelhafeez.librarymanagementsystemapi.service.impl;
 
+import org.abdelhafeez.librarymanagementsystemapi.entity.Book;
+import org.abdelhafeez.librarymanagementsystemapi.entity.BorrowingRecord;
+import org.abdelhafeez.librarymanagementsystemapi.entity.Patron;
 import org.abdelhafeez.librarymanagementsystemapi.exception.BadRequestException;
 import org.abdelhafeez.librarymanagementsystemapi.exception.ResourceNotFoundException;
 import org.abdelhafeez.librarymanagementsystemapi.repo.BorrowingRecordRepo;
 import org.abdelhafeez.librarymanagementsystemapi.service.contract.BookService;
 import org.abdelhafeez.librarymanagementsystemapi.service.contract.BorrowingService;
 import org.abdelhafeez.librarymanagementsystemapi.service.contract.PatronService;
+import org.abdelhafeez.librarymanagementsystemapi.web.dto.ResponseBookDto;
+import org.abdelhafeez.librarymanagementsystemapi.web.dto.ResponsePatronDto;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.AllArgsConstructor;
 
@@ -21,9 +27,25 @@ public class BorrowingServiceImpl implements BorrowingService {
     private final PatronService patronService;
 
     @Override
+    @Transactional(rollbackFor = { ResourceNotFoundException.class, BadRequestException.class, RuntimeException.class,
+            Exception.class })
     public void borrowBook(Long bookId, Long patronId) throws BadRequestException, ResourceNotFoundException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'borrowBook'");
+        ResponseBookDto book = bookService.getBookById(bookId);
+        ResponsePatronDto patron = patronService.getPatronById(patronId);
+        if (!book.getAvailable())
+            throw new BadRequestException("Book Not Returned Yet!");
+        if (!book.getEnabled())
+            throw new BadRequestException("Book Not Enabled!");
+        if (!patron.getEnabled())
+            throw new BadRequestException("Patron Not Enabled!");
+        Book bookEntity = Book.builder().id(bookId).build();
+        Patron patronEntity = Patron.builder().id(patronId).build();
+        BorrowingRecord boorrowEntity = BorrowingRecord.builder()
+                .book(bookEntity)
+                .patron(patronEntity)
+                .build();
+        borrowingRepo.save(boorrowEntity);
+        bookService.makeBookUnAvailable(bookId);
     }
 
     @Override
