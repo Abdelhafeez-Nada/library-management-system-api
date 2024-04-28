adc25739ad87b1d86b32a94b0464577e73ae1669
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-28 22:22:34 +0300

    mock dependencies

--- library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/service/BorrowinServiceTest.java
+++ library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/service/BorrowinServiceTest.java
@@ -1,9 +1,20 @@
 package org.abdelhafeez.librarymanagementsystemapi.service;
 
+import org.abdelhafeez.librarymanagementsystemapi.repo.BorrowingRecordRepo;
+import org.abdelhafeez.librarymanagementsystemapi.service.contract.BookService;
+import org.abdelhafeez.librarymanagementsystemapi.service.contract.PatronService;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class BorrowinServiceTest {
+
+    @Mock
+    private BorrowingRecordRepo borrowingRepo;
+    @Mock
+    private BookService bookService;
+    @Mock
+    private PatronService patronService;
 
 }
