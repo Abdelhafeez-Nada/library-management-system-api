f260d3c728c92fea0b9d4d64ef75bc45221e0862
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-28 18:22:38 +0300

    inject mocks in patron service impl

--- library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/service/PatronServiceTest.java
+++ library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/service/PatronServiceTest.java
@@ -1,8 +1,10 @@
 package org.abdelhafeez.librarymanagementsystemapi.service;
 
 import org.abdelhafeez.librarymanagementsystemapi.repo.PatronRepo;
+import org.abdelhafeez.librarymanagementsystemapi.service.impl.PatronServiceImpl;
 import org.abdelhafeez.librarymanagementsystemapi.util.BeanMapper;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -14,5 +16,8 @@ public class PatronServiceTest {
 
     @Mock
     private PatronRepo patronRepo;
+
+    @InjectMocks
+    private PatronServiceImpl patronServiceImpl;
 
 }
