eb278c9ed0aec6834028b234e612e7efc587d98a
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-28 12:00:45 +0300

    mock bookRepo and beanMapper

--- library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/service/BookServiceTest.java
+++ library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/service/BookServiceTest.java
@@ -1,9 +1,18 @@
 package org.abdelhafeez.librarymanagementsystemapi.service;
 
+import org.abdelhafeez.librarymanagementsystemapi.repo.BookRepo;
+import org.abdelhafeez.librarymanagementsystemapi.util.BeanMapper;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class BookServiceTest {
+
+    @Mock
+    private BookRepo bookRepo;
+
+    @Mock
+    private BeanMapper beanMapper;
 
 }
