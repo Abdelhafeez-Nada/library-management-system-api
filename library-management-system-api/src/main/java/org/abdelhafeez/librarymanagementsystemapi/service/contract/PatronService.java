2a053373f8251ce75ae7394931b9abd153a1e081
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-28 18:00:28 +0300

    add createBook to patron service contract

--- library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/service/contract/PatronService.java
+++ library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/service/contract/PatronService.java
@@ -2,7 +2,9 @@ package org.abdelhafeez.librarymanagementsystemapi.service.contract;
 
 import java.util.List;
 
+import org.abdelhafeez.librarymanagementsystemapi.exception.BadRequestException;
 import org.abdelhafeez.librarymanagementsystemapi.exception.ResourceNotFoundException;
+import org.abdelhafeez.librarymanagementsystemapi.web.dto.RequestPatronDto;
 import org.abdelhafeez.librarymanagementsystemapi.web.dto.ResponsePatronDto;
 import org.springframework.data.domain.Page;
 
@@ -13,4 +15,6 @@ public interface PatronService {
     public Page<ResponsePatronDto> getAllPatrons(int page, int size);
 
     public ResponsePatronDto getPatronById(Long id) throws ResourceNotFoundException;
+
+    public ResponsePatronDto createBook(RequestPatronDto dto) throws BadRequestException;
 }
