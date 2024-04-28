ad8c9d0e3b0fdc12d0fda1d289e067be97cff471
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-28 12:42:21 +0300

    implement GetBookById_EntityFound test

--- library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/service/BookServiceTest.java
+++ library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/service/BookServiceTest.java
@@ -9,6 +9,7 @@ import static org.mockito.Mockito.when;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.abdelhafeez.librarymanagementsystemapi.entity.Book;
 import org.abdelhafeez.librarymanagementsystemapi.repo.BookRepo;
@@ -37,26 +38,19 @@ public class BookServiceTest {
     public void testGetAllBooks() throws Exception {
         // Create a list of mock Book entities
         List<Book> entityList = creatEntityList();
-
         // Stubbing the behavior of the repository to return the mock entity list
         when(bookRepo.findAll()).thenReturn(entityList);
-
         // Stubbing the behavior of the bean mapper to return a list of DTOs
         // based on the entity list provided
         when(beanMapper.mapEntityListToDtoList(entityList, ResponseBookDto.class)).thenReturn(createDtoList());
-
         // Calling the method under test
         List<ResponseBookDto> dtoList = bookServiceImpl.getAllBooks();
-
         // Verifying that bookRepo.findAll() is called exactly once
         verify(bookRepo, times(1)).findAll();
-
         // Asserting that the returned DTO list is not null
         assertNotNull(dtoList);
-
         // Asserting that the size of the DTO list matches the size of the entity list
         assertEquals(entityList.size(), dtoList.size());
-
         // Asserting that each element in the DTO list corresponds to the respective
         // element in the entity list based on ID, title, ISBN, and author
         for (int i = 0; i < entityList.size(); i++) {
@@ -65,6 +59,21 @@ public class BookServiceTest {
             assertEquals(entityList.get(i).getIsbn(), dtoList.get(i).getIsbn());
             assertEquals(entityList.get(i).getAuthor(), dtoList.get(i).getAuthor());
         }
+    }
+
+    @Test
+    public void testGetBookById_EntityFound() throws Exception {
+        Book book = creatEntityList().get(0);
+        // Stubbing repository behavior
+        when(bookRepo.findById(book.getId())).thenReturn(Optional.of(book));
+        // Stubbing mapper behavior
+        when(beanMapper.mapEntityToDto(book, ResponseBookDto.class)).thenReturn(new ResponseBookDto());
+        // Call the method under test
+        ResponseBookDto responseBookDto = bookServiceImpl.getBookById(book.getId());
+        // Verify that repository method was called with the correct ID
+        verify(bookRepo, times(1)).findById(book.getId());
+        // Assert that the returned DTO is not null
+        assertNotNull(responseBookDto);
     }
 
     private List<Book> creatEntityList() {
