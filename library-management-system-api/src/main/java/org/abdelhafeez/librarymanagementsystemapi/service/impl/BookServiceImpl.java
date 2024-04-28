a4ad0a37feae9836785df92310f0723b3dc411a9
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-28 15:15:46 +0300

    add comments

--- library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/service/impl/BookServiceImpl.java
+++ library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/service/impl/BookServiceImpl.java
@@ -23,25 +23,64 @@ public class BookServiceImpl implements BookService {
     private final BookRepo bookRepo;
     private final BeanMapper beanMapper;
 
+    /**
+     * Retrieves all books available in the system.
+     * 
+     * @return A list of ResponseBookDto representing all books.
+     */
     @Override
     public List<ResponseBookDto> getAllBooks() {
-        return beanMapper.mapEntityListToDtoList(bookRepo.findAll(), ResponseBookDto.class);
+        // Retrieve all books from the repository
+        List<Book> allBooks = bookRepo.findAll();
+        // Map the list of book entities to a list of DTOs and return it
+        return beanMapper.mapEntityListToDtoList(allBooks, ResponseBookDto.class);
     }
 
+    /**
+     * Retrieves a book by its ID.
+     * 
+     * @param id The ID of the book to retrieve.
+     * @return The ResponseBookDto representing the book.
+     * @throws ResourceNotFoundException If the book with the given ID is not found.
+     */
     @Override
     public ResponseBookDto getBookById(Long id) throws ResourceNotFoundException {
+        // Retrieve book from the repository by ID
         Optional<Book> optional = bookRepo.findById(id);
-        if (!optional.isPresent())
-            throw new ResourceNotFoundException("Book", "Id", id);
-        return beanMapper.mapEntityToDto(optional.get(), ResponseBookDto.class);
+        // Map the book to a DTO if it exists, otherwise throw a
+        // ResourceNotFoundException
+        return optional.map(
+                book -> beanMapper.mapEntityToDto(book, ResponseBookDto.class))
+                .orElseThrow(() -> new ResourceNotFoundException("Book", "Id", id));
     }
 
+    /**
+     * Creates a new book.
+     * 
+     * @param dto The RequestBookDto containing information about the book to be
+     *            created.
+     * @return The ResponseBookDto representing the newly created book.
+     * @throws BadRequestException If the request is malformed or contains invalid
+     *                             data.
+     */
     @Override
     public ResponseBookDto createBook(RequestBookDto dto) throws BadRequestException {
+        // Save the book entity to the repository
         Book saved = bookRepo.save(beanMapper.mapDtoToEntity(dto, Book.class));
+        // Map the saved book entity to a DTO and return it
         return beanMapper.mapEntityToDto(saved, ResponseBookDto.class);
     }
 
+    /**
+     * Updates the details of a book with the specified ID.
+     *
+     * @param id  The ID of the book to be updated.
+     * @param dto The RequestBookDto containing the updated information of the book.
+     * @return The ResponseBookDto representing the updated book.
+     * @throws ResourceNotFoundException If the book with the given ID is not found.
+     * @throws BadRequestException       If the request is malformed or contains
+     *                                   invalid data.
+     */
     @Override
     @Transactional(rollbackFor = { ResourceNotFoundException.class, BadRequestException.class })
     public ResponseBookDto updateBook(Long id, RequestBookDto dto)
@@ -53,18 +92,27 @@ public class BookServiceImpl implements BookService {
         Optional<Book> optionalBook = bookRepo.findById(id);
         if (!optionalBook.isPresent())
             throw new ResourceNotFoundException("Book", "Id", id);
-        // Update Entity
+        // Retrieve the book entity from the repository
         Book book = optionalBook.get();
+        // Update the book entity with the new information
         book.setTitle(dto.getTitle());
         book.setAuthor(dto.getAuthor());
         book.setIsbn(dto.getIsbn());
         book.setPublicationYear(dto.getPublicationYear());
-        // Save Changes
+        // Save the changes to the repository
         Book updatedBook = bookRepo.save(book);
-        // Map Entity to DTO
+        // Map the updated book entity to a DTO and return it
         return beanMapper.mapEntityToDto(updatedBook, ResponseBookDto.class);
     }
 
+    /**
+     * Deletes a book with the specified ID.
+     *
+     * @param id The ID of the book to be deleted.
+     * @throws ResourceNotFoundException If the book with the given ID is not found.
+     * @throws BadRequestException       If the request is malformed or contains
+     *                                   invalid data.
+     */
     @Override
     public void deleteBook(Long id) throws ResourceNotFoundException, BadRequestException {
         // Input Validation
@@ -74,6 +122,8 @@ public class BookServiceImpl implements BookService {
         Optional<Book> optionalBook = bookRepo.findById(id);
         if (!optionalBook.isPresent())
             throw new ResourceNotFoundException("Book", "Id", id);
+        // Delete the book entity from the repository
         bookRepo.deleteById(id);
     }
+
 }
