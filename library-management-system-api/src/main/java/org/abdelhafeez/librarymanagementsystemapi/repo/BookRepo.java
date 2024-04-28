35fb73bcde559076b940bb9f1e47155844dcd156
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-28 20:50:14 +0300

    implement makeBookUnAvailable in book repo

--- library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/repo/BookRepo.java
+++ library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/repo/BookRepo.java
@@ -2,9 +2,17 @@ package org.abdelhafeez.librarymanagementsystemapi.repo;
 
 import org.abdelhafeez.librarymanagementsystemapi.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface BookRepo extends JpaRepository<Book, Long> {
 
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true, value = "update books set available=false, updated_at=current_timestamp where id=:bookId")
+    public int makeBookUnAvailable(@Param("bookId") long bookId);
 }
