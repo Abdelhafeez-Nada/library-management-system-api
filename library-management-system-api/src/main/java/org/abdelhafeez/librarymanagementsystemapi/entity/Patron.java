6717f9bd0ec309d1246766d57323177a5abdedb7
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-28 19:13:21 +0300

    add enabled to patron

--- library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/entity/Patron.java
+++ library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/entity/Patron.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -31,6 +32,8 @@ public class Patron {
     private Long id;
     private String name;
     private String contactInfo;
+    @Column(columnDefinition = "BOOLEAN DEFAULT true")
+    private Boolean enabled;
     @OneToMany(mappedBy = "patron", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<BorrowingRecord> borrowingRecords;
     @CreationTimestamp
