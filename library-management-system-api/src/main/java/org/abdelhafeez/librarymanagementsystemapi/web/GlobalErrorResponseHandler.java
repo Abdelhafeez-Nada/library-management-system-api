a7dcfa6f065fff88b78758e83bd1fa52f2742f76
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-29 14:41:48 +0300

    create GlobalErrorResponseHandler class

---
+++ library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/web/GlobalErrorResponseHandler.java
@@ -0,0 +1,8 @@
+package org.abdelhafeez.librarymanagementsystemapi.web;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalErrorResponseHandler {
+
+}
