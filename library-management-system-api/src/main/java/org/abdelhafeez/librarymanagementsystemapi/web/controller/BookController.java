b2a1fc26c47b2c99876c472eddbc4c55047f131b
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-29 12:38:19 +0300

    inject Book Service

--- library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/web/controller/BookController.java
+++ library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/web/controller/BookController.java
@@ -1,10 +1,15 @@
 package org.abdelhafeez.librarymanagementsystemapi.web.controller;
 
+import org.abdelhafeez.librarymanagementsystemapi.service.contract.BookService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
+
 @RestController
 @RequestMapping("api/books")
+@AllArgsConstructor
 public class BookController {
 
+    private final BookService bookService;
 }
