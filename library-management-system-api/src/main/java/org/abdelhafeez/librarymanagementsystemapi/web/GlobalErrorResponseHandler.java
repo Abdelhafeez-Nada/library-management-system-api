03187ca5def5fdf9ebd00dd87bc6e2bec409336a
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-29 14:53:56 +0300

    implement ResourceNotFoundException handler

--- library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/web/GlobalErrorResponseHandler.java
+++
@@ -1,8 +0,0 @@
-package org.abdelhafeez.librarymanagementsystemapi.web;
-
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
-public class GlobalErrorResponseHandler {
-
-}
---
+++ library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/web/errorresponsehandler/GlobalErrorResponseHandler.java
@@ -0,0 +1,30 @@
+package org.abdelhafeez.librarymanagementsystemapi.web.errorresponsehandler;
+
+import org.abdelhafeez.librarymanagementsystemapi.exception.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalErrorResponseHandler {
+
+    /**
+     * Handles ResourceNotFoundException and returns a ResponseEntity with an
+     * ErrorResponseDto containing
+     * error details and HTTP status 404 Not Found.
+     * 
+     * @param exception The ResourceNotFoundException to handle.
+     * @return ResponseEntity containing an ErrorResponseDto representing the error
+     *         and HttpStatus.NOT_FOUND.
+     */
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponseDto> resourceNotFoundException(ResourceNotFoundException exception) {
+        // Create an ErrorResponseDto with error details
+        ErrorResponseDto dto = new ErrorResponseDto("404", exception.getMessage());
+        // Return a ResponseEntity with the ErrorResponseDto and HTTP status 404 Not
+        // Found
+        return new ResponseEntity<>(dto, HttpStatus.NOT_FOUND);
+    }
+
+}
