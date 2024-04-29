d679d2dad878565d5a9e15897223e726cc0a5ce3
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-29 15:00:07 +0300

    add comments and docs

--- library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/web/errorresponsehandler/GlobalErrorResponseHandler.java
+++ library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/web/errorresponsehandler/GlobalErrorResponseHandler.java
@@ -28,12 +28,21 @@ public class GlobalErrorResponseHandler {
         return new ResponseEntity<>(dto, HttpStatus.NOT_FOUND);
     }
 
+    /**
+     * Handles BadRequestException and returns a ResponseEntity with an
+     * ErrorResponseDto containing
+     * error details and HTTP status 400 Bad Request.
+     * 
+     * @param exception The BadRequestException to handle.
+     * @return ResponseEntity containing an ErrorResponseDto representing the error
+     *         and HttpStatus.BAD_REQUEST.
+     */
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<ErrorResponseDto> badRequesException(BadRequestException exception) {
+    public ResponseEntity<ErrorResponseDto> badRequestException(BadRequestException exception) {
         // Create an ErrorResponseDto with error details
         ErrorResponseDto dto = new ErrorResponseDto("400", exception.getMessage());
-        // Return a ResponseEntity with the ErrorResponseDto and HTTP status 404 Not
-        // Found
+        // Return a ResponseEntity with the ErrorResponseDto and HTTP status 400 Bad
+        // Request
         return new ResponseEntity<>(dto, HttpStatus.BAD_REQUEST);
     }
 
