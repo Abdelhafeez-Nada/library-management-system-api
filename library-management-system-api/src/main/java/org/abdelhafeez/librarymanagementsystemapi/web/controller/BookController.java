0a6f238aa533fed78dc3a8981ef19b0ef7d49036
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-29 13:33:07 +0300

    implement get all books endpoint

--- library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/web/controller/BookController.java
+++ library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/web/controller/BookController.java
@@ -1,6 +1,12 @@
 package org.abdelhafeez.librarymanagementsystemapi.web.controller;
 
+import java.util.List;
+
 import org.abdelhafeez.librarymanagementsystemapi.service.contract.BookService;
+import org.abdelhafeez.librarymanagementsystemapi.web.dto.ResponseBookDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +18,19 @@ import lombok.AllArgsConstructor;
 public class BookController {
 
     private final BookService bookService;
+
+    /**
+     * Retrieves all books.
+     * 
+     * @return ResponseEntity containing a list of ResponseBookDto objects
+     *         representing the books and HttpStatus.OK
+     */
+    @GetMapping()
+    public ResponseEntity<List<ResponseBookDto>> getAllBooks() {
+        // Call the service layer to retrieve all books
+        List<ResponseBookDto> list = bookService.getAllBooks();
+        // Return a ResponseEntity with the list of books and HTTP status OK
+        return new ResponseEntity<List<ResponseBookDto>>(list, HttpStatus.OK);
+    }
+
 }
