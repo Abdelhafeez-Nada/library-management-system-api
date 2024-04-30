3f3743ee61e47a96e93d93dca3ef678e46888ba2
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-30 11:11:03 +0300

    mock bean of BookService

--- library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/controller/BookControllerTest.java
+++ library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/controller/BookControllerTest.java
@@ -1,9 +1,13 @@
 package org.abdelhafeez.librarymanagementsystemapi.controller;
 
+import org.abdelhafeez.librarymanagementsystemapi.service.contract.BookService;
 import org.abdelhafeez.librarymanagementsystemapi.web.controller.BookController;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @WebMvcTest(BookController.class)
 public class BookControllerTest {
 
+    @MockBean
+    private BookService bookService;
 }
