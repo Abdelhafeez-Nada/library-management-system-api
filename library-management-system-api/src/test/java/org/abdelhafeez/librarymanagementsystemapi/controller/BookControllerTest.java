fbca2b45875ad67dce58dc30dff72eb71b5c9ed6
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-30 11:10:08 +0300

    create BookControllerTest class

---
+++ library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/controller/BookControllerTest.java
@@ -0,0 +1,9 @@
+package org.abdelhafeez.librarymanagementsystemapi.controller;
+
+import org.abdelhafeez.librarymanagementsystemapi.web.controller.BookController;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(BookController.class)
+public class BookControllerTest {
+
+}
