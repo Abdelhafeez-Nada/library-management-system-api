ae21e3bf2d8b0e3601a0fb018f03aabac5eb8b73
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-30 12:27:51 +0300

    injectmock mvc

--- library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/controller/BookControllerTest.java
+++ library-management-system-api/src/test/java/org/abdelhafeez/librarymanagementsystemapi/controller/BookControllerTest.java
@@ -2,12 +2,18 @@ package org.abdelhafeez.librarymanagementsystemapi.controller;
 
 import org.abdelhafeez.librarymanagementsystemapi.service.contract.BookService;
 import org.abdelhafeez.librarymanagementsystemapi.web.controller.BookController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(BookController.class)
 public class BookControllerTest {
 
+    private static final String ENDPOINT_PATH = "/api/books";
+    @Autowired
+    private MockMvc mockMvc;
     @MockBean
     private BookService bookService;
+
 }
