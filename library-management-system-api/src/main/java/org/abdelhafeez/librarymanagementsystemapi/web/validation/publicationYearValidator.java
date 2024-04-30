5538a2dee117bd54bd21fb791d8c69d82334ed71
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-04-30 12:26:35 +0300

    handle null values

--- library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/web/validation/publicationYearValidator.java
+++ library-management-system-api/src/main/java/org/abdelhafeez/librarymanagementsystemapi/web/validation/publicationYearValidator.java
@@ -9,6 +9,8 @@ public class publicationYearValidator implements ConstraintValidator<Publication
 
     @Override
     public boolean isValid(Short value, ConstraintValidatorContext context) {
+        if (value == null)
+            return false;
         Short start = Short.valueOf(String.valueOf(1900));
         Short end = Short.valueOf(String.valueOf(LocalDate.now().getYear()));
         return value >= start && value <= end;
